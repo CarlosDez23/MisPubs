c590ee142d1105c9bf0e076f93b5d3c5e7098d41
CarlosDez23 <52250143+CarlosDez23@users.noreply.github.com>
2020-01-25 16:52:21 +0100

    Terminado layout del login

--- app/src/main/java/com/example/mispubs/ActivityLogin.java
+++ app/src/main/java/com/example/mispubs/ActivityLogin.java
@@ -11,8 +11,11 @@ public class ActivityLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         //Lo ponemos a pantalla completa
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.getSupportActionBar().hide();
     }
+
+
 }
