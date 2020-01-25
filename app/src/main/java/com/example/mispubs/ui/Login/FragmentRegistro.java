1c0694f99ac2d4e5f44dfd8ea541c3ae5e7f21a3
CarlosDez23 <52250143+CarlosDez23@users.noreply.github.com>
2020-01-25 19:30:39 +0100

    Inicio de la rama para el registro

---
+++ app/src/main/java/com/example/mispubs/ui/Login/FragmentRegistro.java
@@ -0,0 +1,36 @@
+package com.example.mispubs.ui.Login;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mispubs.R;
+
+public class FragmentRegistro extends Fragment {
+
+
+    public static FragmentRegistro newInstance() {
+        return new FragmentRegistro();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_registro_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+
+}
