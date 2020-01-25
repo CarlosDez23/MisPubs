4166151e84f62db7e2e43b642174aa57800ff547
CarlosDez23 <52250143+CarlosDez23@users.noreply.github.com>
2020-01-25 14:12:05 +0100

    Layout del Login

---
+++ app/src/main/java/com/example/mispubs/ActivityLogin.java
@@ -0,0 +1,18 @@
+package com.example.mispubs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        //Lo ponemos a pantalla completa
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getSupportActionBar().hide();
+    }
+}
