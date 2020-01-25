d730beeabb24ff0d51a6187b80e09946a8eab872
Makenrou <52251013+Makenrou@users.noreply.github.com>
2020-01-25 18:42:27 +0100

    Primera Prueba Login

--- app/src/main/java/com/example/mispubs/ui/Login/ActivityLogin.java
+++ app/src/main/java/com/example/mispubs/ui/Login/ActivityLogin.java
@@ -2,15 +2,24 @@ package com.example.mispubs.ui.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.example.mispubs.MainActivity;
+import com.example.mispubs.Modelo.Usuario;
 import com.example.mispubs.R;
+import com.example.mispubs.REST.UsuarioRest;
 import com.google.android.material.snackbar.Snackbar;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ActivityLogin extends AppCompatActivity {
 
@@ -19,7 +28,13 @@ public class ActivityLogin extends AppCompatActivity {
     private TextView tvLoginRegistro;
     private RelativeLayout relativeLoginBoton;
 
+    //Para utilizarlo en el REST
+    private UsuarioRest usuarioRest;
 
+
+    private ActivityLogin getActividad(){
+        return this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,15 +70,38 @@ public class ActivityLogin extends AppCompatActivity {
                     Snackbar.make(v, "Registro", Snackbar.LENGTH_LONG).show();
                     break;
                 case R.id.relativeLoginBoton:
-                    Snackbar.make(v,"Botón entrar",Snackbar.LENGTH_LONG).show();
+                    //Snackbar.make(v,"Botón entrar",Snackbar.LENGTH_LONG).show();
+                    comprobarUsuario();
                     break;
                 default:
                     break;
             }
-
-
         }
     };
+
+    private void comprobarUsuario(){
+
+        Call<Usuario> call = usuarioRest.buscarPorCorreoPass(etLoginEmail.getText().toString(),etLoginPasssword.getText().toString());
+
+        call.enqueue(new Callback<Usuario>() {
+            @Override
+            public void onResponse(Call<Usuario> call, Response<Usuario> response) {
+                if (response.isSuccessful()){
+                    if (response.code() == 200){
+                        Snackbar.make(getActividad().getWindow().getDecorView().getRootView(), "TODO OK", Snackbar.LENGTH_LONG).show();
+                        startActivity(new Intent(getActividad(), MainActivity.class));
+                    }else{
+                        Snackbar.make(getActividad().getWindow().getDecorView().getRootView(), "NO REGISTRADO", Snackbar.LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Usuario> call, Throwable t) {
+                Log.e("ERROR: ", t.getMessage());
+            }
+        });
+    }
 
 
 }
