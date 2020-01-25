55e08c504e68dfd1afc3f347f5e5597cc071beb1
Makenrou <52251013+Makenrou@users.noreply.github.com>
2020-01-25 16:48:50 +0100

    Commit SplashScreen

---
+++ app/src/main/java/com/example/mispubs/ActivitySplash.java
@@ -0,0 +1,29 @@
+package com.example.mispubs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(ActivitySplash.this, ActivityLogin.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);//tiempo que debe estar ejecutandose
+    }
+}
