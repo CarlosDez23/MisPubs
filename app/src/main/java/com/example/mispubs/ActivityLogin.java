bb90101e45ec23bdd4e08188e5d8ddb431499f20
CarlosDez23 <52250143+CarlosDez23@users.noreply.github.com>
2020-01-25 17:11:38 +0100

    Vistas llamadas y listeners seteados

--- app/src/main/java/com/example/mispubs/ActivityLogin.java
+++ app/src/main/java/com/example/mispubs/ActivityLogin.java
@@ -3,9 +3,22 @@ package com.example.mispubs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class ActivityLogin extends AppCompatActivity {
+
+    private EditText etLoginEmail;
+    private EditText etLoginPasssword;
+    private TextView tvLoginRegistro;
+    private RelativeLayout relativeLoginBoton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +28,40 @@ public class ActivityLogin extends AppCompatActivity {
         //Lo ponemos a pantalla completa
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.getSupportActionBar().hide();
+
+        llamarVistas();
     }
+
+
+    private void llamarVistas(){
+
+        this.etLoginEmail = findViewById(R.id.etLoginEmail);
+        this.etLoginPasssword = findViewById(R.id.etLoginPassword);
+        this.tvLoginRegistro = findViewById(R.id.tvLoginRegistro);
+        this.relativeLoginBoton = findViewById(R.id.relativeLoginBoton);
+
+        tvLoginRegistro.setOnClickListener(listenerBotones);
+        relativeLoginBoton.setOnClickListener(listenerBotones);
+
+    }
+
+    private View.OnClickListener listenerBotones = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()){
+                case R.id.tvLoginRegistro:
+                    Snackbar.make(v, "Registro", Snackbar.LENGTH_LONG).show();
+                    break;
+                case R.id.relativeLoginBoton:
+                    Snackbar.make(v,"Botón entrar",Snackbar.LENGTH_LONG).show();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    };
 
 
 }
