25bf67a14ceb873e79986d0e50368ac2324db8b7
CarlosDez23 <52250143+CarlosDez23@users.noreply.github.com>
2020-01-27 20:22:42 +0100

    Merge rama pubs

--- app/src/main/java/com/example/mispubs/MainActivity.java
+++ app/src/main/java/com/example/mispubs/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navView, navController);
 
         usuario = (Usuario) this.getIntent().getExtras().getSerializable("usuario");
+        System.out.println("Merge pubs Carlos");
     }
 
 }
