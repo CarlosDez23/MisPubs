bf9ba96264af0ba5544cffc108c1eb0644e94eda
Makenrou <52251013+Makenrou@users.noreply.github.com>
2020-01-30 12:27:24 +0100

    Merge Adrian

--- app/src/main/java/com/example/mispubs/MainActivity.java
+++ app/src/main/java/com/example/mispubs/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         pedirPermisos();
 
         usuario = (Usuario) this.getIntent().getExtras().getSerializable("usuario");
-        System.out.println("MAPAS ");
+        System.out.println("FIN INTERFAZ");
     }
 
     private void pedirPermisos(){
