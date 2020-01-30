d2f1eabc3d656791acea6bd50f11b0abf49ac82c
Makenrou <52251013+Makenrou@users.noreply.github.com>
2020-01-30 20:00:21 +0100

    Merge AdrianPUBS

--- app/src/main/java/com/example/mispubs/MainActivity.java
+++ app/src/main/java/com/example/mispubs/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         pedirPermisos();
 
         usuario = (Usuario) this.getIntent().getExtras().getSerializable("usuario");
-        System.out.println("FIN INTERFAZ");
+        System.out.println("MODOS PUBS");
     }
 
     private void pedirPermisos(){
