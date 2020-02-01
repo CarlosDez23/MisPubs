258a55c05ace6785711439457974554103865721
Makenrou <52251013+Makenrou@users.noreply.github.com>
2020-02-01 17:36:57 +0100

    Merge insertar eliminar y modificar pubs finalizado

--- app/src/main/java/com/example/mispubs/MainActivity.java
+++ app/src/main/java/com/example/mispubs/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         pedirPermisos();
 
         usuario = (Usuario) this.getIntent().getExtras().getSerializable("usuario");
-        System.out.println("AÑADIR PUB");
+        System.out.println("Pub");
     }
 
     private void pedirPermisos(){
